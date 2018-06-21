87bdad00bd8d5a3da2873cc1a2cbe608f801b0b5
MaximKiselyow <m.kiselyow@gmail.com>
2018-06-21 09:43:12 +0300

    devise sign_in&sign_up tests, helper fix

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   it { should have_db_column(:email).with_options(null: false) }
-  it { should have_db_column(:firstname).with_options(null: false) }
-  it { should have_db_column(:lastname).with_options(null: false) }
+  it { should have_db_column(:firstname) }
+  it { should have_db_column(:lastname) }
   it { should have_many(:orders) }
   it { should have_many(:reviews) }
   describe "validations" do
