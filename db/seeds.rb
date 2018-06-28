979cc2b414a43cc8b88db65e2aa4c6229e0f3fa9
MaximKiselyow <m.kiselyow@gmail.com>
2018-06-29 00:00:19 +0300

    add factory, that generates all entities and change seeds rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,19 @@ Faker::UniqueGenerator.clear
 Book.all.each(&:destroy)
 Author.all.each(&:destroy)
 Category.all.each(&:destroy)
-FactoryBot.create_list(:book, 20)
-password = "foobar"
 Customer.all.each(&:destroy)
-FactoryBot.create_list(:customer, 5, password: password, password_confirmation: password)
 AdminUser.all.each(&:destroy)
+
+['logged in', 'guest'].each do |role|
+  Role.find_or_create_by({name: role})
+end
+
+['Mobile development', 'Photo', 'Web design', 'Web development'].each do |category_title|
+  Category.find_or_create_by({title: category_title})
+end
+
+FactoryBot.create_list(:customer_with_everything, 10)
 AdminUser.create!(
   email: ACTIVE_ADMIN_LOGIN, 
   password: ACTIVE_ADMIN_PASSWORD, 
   password_confirmation: ACTIVE_ADMIN_PASSWORD) if Rails.env.development? || Rails.env.test?
-['logged in', 'guest'].each do |role|
-  Role.find_or_create_by({name: role})
-end
