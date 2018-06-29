117d3f008c5289c6fdc9fadfded15413c00131f3
MaximKiselyow <m.kiselyow@gmail.com>
2018-06-29 09:41:07 +0300

    guest user email regex test

--- spec/integration/guest_user_spec.rb
+++ spec/integration/guest_user_spec.rb
@@ -2,12 +2,8 @@ require 'rails_helper'
 
 describe "guest_customer", :type => :feature do
   it "have been created on not-authorized visit" do
-    # expect{ visit '/' }.to change{ page.get_rack_session_key('guest_customer') }
     visit '/'
     expect(page.get_rack_session_key('guest_customer')).not_to eq(nil)
+    expect(page.get_rack_session_key('guest_customer').to_hash["email"]).to match(/guest_customer_\d{12}@example.com/)
   end
-
-  # it "have been created on not-authorized visit" do
-  #   visit '/'
-  # end
 end
