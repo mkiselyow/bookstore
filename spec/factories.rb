d245bc8b7b48b05a82186aa092721421ceb1441d
MaximKiselyow <m.kiselyow@gmail.com>
2018-07-13 16:27:55 +0300

    rewiews factory added

--- spec/factories.rb
+++ spec/factories.rb
@@ -26,7 +26,7 @@ FactoryBot.define do
     sold           {Faker::Number.between(0, 10)}
     books_in_stock {Faker::Number.between(1, 25)}
     association :author, factory: :author
-    category_id {Category.all.count > 3 ? Category.all.sample.id : FactoryBot.create(:category).id}
+    category_id {Category.count > 3 ? Category.all.sample.id : FactoryBot.create(:category).id}
     description {Faker::Lorem.paragraph(15, true, 10)}
   end
 
@@ -94,6 +94,14 @@ FactoryBot.define do
       create_list :order_with_order_items, 3,
        customer: customer_with_everything,
         credit_card: customer_with_everything.credit_cards.first
+      create_list :review, (Book.count*2)
     end
+  end
+
+  factory :review, class: 'Review' do
+    text_review   {Faker::Lorem.paragraph(2, true, 6)}
+    rating_number {Faker::Number.between(1, 10)}
+    customer_id       {Customer.count > 1 ? Customer.all.sample.id : FactoryBot.create(:customer).id}
+    book_id   {Book.count > 1 ? Book.all.sample.id : FactoryBot.create(:book).id}
   end
 end
